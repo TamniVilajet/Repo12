9086eef2810122bd3622076a518d9a1435d0ec31
TamniVilajet <ermin.kirlic@gmail.com>
2022-04-08 22:54:43 +0200

    Exercise 03

---
+++ ex03/src/index.js
@@ -0,0 +1,20 @@
+// Only change code below this line
+function rangeOfNumbers(startN, endN) {
+    if(startN > endN){
+      return ["The starting number will always be less than or equal to the ending number"]
+  } else {
+  if(startN == endN){
+      return [startN]
+    }
+  var myArray = rangeOfNumbers(startN, endN - 1)
+  myArray.push(endN)
+  return myArray
+  }
+}     
+// Only change code above this line
+console.log(rangeOfNumbers(1,7));  
+console.log(rangeOfNumbers(3,10));
+console.log(rangeOfNumbers(5,5)); 
+console.log(rangeOfNumbers(7,5));       
+module.exports = rangeOfNumbers;
+
